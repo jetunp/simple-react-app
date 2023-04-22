5a445acbd85885ed26f3bde59af2f5ac1182b67a
jetun patel <jetun.jp@gmail.com>
2023-04-22 19:10:59 -0400

    Adding JSON-server mock to act as a backend for the data to persist on the server

--- src/App.js
+++ src/App.js
@@ -1,51 +1,79 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 const App = () => {
   //global level state to toggle the form to add a new task
   const [showAddTask, setShowAddTask ] = useState(false)
 
   //global level state to manipulate with the tasks
-  const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 4th at 1pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'School Meeting',
-        day: 'May 6th at 7pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Grocery Shopping',
-        day: 'Feb 12th at 9am',
-        reminder: false,
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
     }
-])
+    getTasks()
+  }, [])
+
+//fetch tasks
+const fetchTasks = async() => {
+  const res = await fetch('http://localhost:5000/tasks')
+  const data = await res.json()
+  return data
+}
+
+//This will get us individual tasks so we can use it for like togg;e between reminder set or not using toggleReminder function.
+//basically just an update to the indivdual task that we can use it for.
+//Fetch task
+const fetchTask = async(id) => {
+  const res = await fetch(`http://localhost:5000/tasks/${id}`)
+  const data = await res.json()
+  return data
+}
 
 
 //Add Tasks
-const addTask = (task) => {
-    //generate a random id to be attached with any new task being added
-    const id = Math.floor(Math.random()*10000) +1
-    const newTask = {id, ...task }
-    setTasks([...tasks, newTask])
+const addTask = async(task) => {
+    //adds the tasks not only to the browser but to theserver as well.
+    const res = await fetch(`http://localhost:5000/tasks`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+    setTasks([...tasks, data])
 }
 
 //Delete task
-const deleteTask = (id) => {
+const deleteTask = async (id) => {
+    //deletes the tasks not only from the browser from the server as well.
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
     setTasks(tasks.filter((task) => task.id !== id))
 }
 
 //Toggle reminder and highlight if true
-const toggleReminder = (id) => {
-     setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task
+const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder}
+    const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+        method: 'PUT',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(updatedTask)
+    })
+    const data = await res.json()
+    setTasks(tasks.map((task) => 
+      task.id === id ? {...task, reminder: 
+      data.reminder} : task
 ))}
 
 
